081c1a9183ca7349e541cd4ce4829a777a0c9445
Slava Akhmechet <coffeemug@gmail.com>
2007-03-15 03:03:00 +0000

    Fixing up unit tests
    darcs-hash:20070315030337-b0caf-4a016704433636d6aa902193291f4ef0b950745c.gz

--- test/renderers/data-renderer.lisp
+++ test/renderers/data-renderer.lisp
@@ -4,13 +4,14 @@
 ;;; test with-data-header
 (deftest-html with-data-header-1
     (with-data-header *joe* (lambda () nil))
-  (:div :class "data employee"
+  (:div :class "renderer data employee"
 	(:div :class "extra-top-1" "&nbsp;")
 	(:div :class "extra-top-2" "&nbsp;")
 	(:div :class "extra-top-3" "&nbsp;")
-	(:h1 (:span :class "action" "Viewing:&nbsp;")
-	     (:span :class "object" "Employee"))
-	(:ul nil)
+	(:div :class "fields"
+	 (:h1 (:span :class "action" "Viewing:&nbsp;")
+	      (:span :class "object" "Employee"))
+	 (:ul nil))
 	(:div :class "extra-bottom-1" "&nbsp;")
 	(:div :class "extra-bottom-2" "&nbsp;")
 	(:div :class "extra-bottom-3" "&nbsp;")))
@@ -18,67 +19,79 @@
 ;;; test render-data-slot
 (deftest-html render-data-slot-1
     (render-data-slot *joe* 'first-name "Joe")
-  (:li (:h2 "First Name:") (:span "Joe")))
+  (:li (:span :class "label"
+	      "First Name:&nbsp;")
+       (:span :class "value"
+	      "Joe")))
 
 (deftest-html render-data-slot-2
     (render-data-slot *joe* 'address-ref *home-address*)
-  (:li (:h2 "Address:") (:span "ADDRESS")))
+  (:li (:span :class "label"
+	      "Address:&nbsp;")
+       (:span :class "value"
+	      "ADDRESS")))
 
 (deftest-html render-data-slot-3
     (render-data-slot *joe* 'education *some-college*)
   (htm
-   (:li (:h2 "University:") (:span "Bene Gesserit University"))
-   (:li (:h2 "Graduation Year:") (:span "2000"))))
+   (:li (:span :class "label" "University:&nbsp;")
+	(:span :class "value" "Bene Gesserit University"))
+   (:li (:span :class "label" "Graduation Year:&nbsp;")
+	(:span :class "value" "2000"))))
 
 ;;; test render-data
 (deftest-html render-data-1
     (render-data "test")
-  (:span "test"))
+  (:span :class "value" "test"))
 
 (deftest-html render-data-2
     (render-data *joe*)
-  (:div :class "data employee"
+  (:div :class "renderer data employee"
 	(:div :class "extra-top-1" "&nbsp;")
 	(:div :class "extra-top-2" "&nbsp;")
 	(:div :class "extra-top-3" "&nbsp;")
-	(:h1 (:span :class "action" "Viewing:&nbsp;")
-	     (:span :class "object" "Employee"))
-	(:ul
-	 (:li (:h2 "Name:") (:span "Joe"))
-	 (:li (:h2 "Manager:") (:span "Jim")))
+	(:div :class "fields"
+	      (:h1 (:span :class "action" "Viewing:&nbsp;")
+		   (:span :class "object" "Employee"))
+	      (:ul
+	       (:li (:span :class "label" "Name:&nbsp;") (:span :class "value" "Joe"))
+	       (:li (:span :class "label" "Manager:&nbsp;") (:span :class "value" "Jim"))))
 	(:div :class "extra-bottom-1" "&nbsp;")
 	(:div :class "extra-bottom-2" "&nbsp;")
 	(:div :class "extra-bottom-3" "&nbsp;")))
 
 (deftest-html render-data-3
     (render-data *joe* :slots '(address-ref))
-  (:div :class "data employee"
+  (:div :class "renderer data employee"
 	(:div :class "extra-top-1" "&nbsp;")
 	(:div :class "extra-top-2" "&nbsp;")
 	(:div :class "extra-top-3" "&nbsp;")
-	(:h1 (:span :class "action" "Viewing:&nbsp;")
-	     (:span :class "object" "Employee"))
-	(:ul
-	 (:li (:h2 "Name:") (:span "Joe"))
-	 (:li (:h2 "Address:") (:span "ADDRESS"))
-	 (:li (:h2 "Manager:") (:span "Jim")))
+	(:div :class "fields"
+	      (:h1 (:span :class "action" "Viewing:&nbsp;")
+		   (:span :class "object" "Employee"))
+	      (:ul
+	       (:li (:span :class "label" "Name:&nbsp;") (:span :class "value" "Joe"))
+	       (:li (:span :class "label" "Address:&nbsp;") (:span :class "value" "ADDRESS"))
+	       (:li (:span :class "label" "Manager:&nbsp;") (:span :class "value" "Jim"))))
 	(:div :class "extra-bottom-1" "&nbsp;")
 	(:div :class "extra-bottom-2" "&nbsp;")
 	(:div :class "extra-bottom-3" "&nbsp;")))
 
 (deftest-html render-data-4
     (render-data *joe* :slots '(education))
-  (:div :class "data employee"
+  (:div :class "renderer data employee"
 	(:div :class "extra-top-1" "&nbsp;")
 	(:div :class "extra-top-2" "&nbsp;")
 	(:div :class "extra-top-3" "&nbsp;")
-	(:h1 (:span :class "action" "Viewing:&nbsp;")
-	     (:span :class "object" "Employee"))
-	(:ul
-	 (:li (:h2 "Name:") (:span "Joe"))
-	 (:li (:h2 "University:") (:span "Bene Gesserit University"))
-	 (:li (:h2 "Graduation Year:") (:span "2000"))
-	 (:li (:h2 "Manager:") (:span "Jim")))
+	(:div :class "fields"
+	      (:h1 (:span :class "action" "Viewing:&nbsp;")
+		   (:span :class "object" "Employee"))
+	      (:ul
+	       (:li (:span :class "label" "Name:&nbsp;") (:span :class "value" "Joe"))
+	       (:li (:span :class "label" "University:&nbsp;")
+		    (:span :class "value" "Bene Gesserit University"))
+	       (:li (:span :class "label" "Graduation Year:&nbsp;") (:span :class "value" "2000"))
+	       (:li (:span :class "label" "Manager:&nbsp;") (:span :class "value" "Jim"))))
 	(:div :class "extra-bottom-1" "&nbsp;")
 	(:div :class "extra-bottom-2" "&nbsp;")
 	(:div :class "extra-bottom-3" "&nbsp;")))
