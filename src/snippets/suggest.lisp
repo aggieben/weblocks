79e92edff6028b1fba888a5a571ec7a75195364e
Slava Akhmechet <coffeemug@gmail.com>
2007-08-21 06:42:00 +0000

    Adding us-state datatype, properly specializing form for it, modifying suggest, adding dropdown snippet
    darcs-hash:20070821064203-b0caf-4fde8d2c0362634f30332599e2da4e561a381835.gz

--- src/snippets/suggest.lisp
+++ src/snippets/suggest.lisp
@@ -3,57 +3,67 @@
 
 (export '(render-suggest))
 
-(defun render-suggest (input-name localp &key value fetch-fn
+(defun render-suggest (input-name resultset &key
+		       default-value
 		       (input-id (gensym))
 		       (choices-id (gensym))
-		       (format-fn #'format-suggest-list))
+		       (format-fn #'format-suggest-list)
+		       (max-length *max-raw-input-length*)
+		       welcome-name)
   "Renders a block that provides functionality similar to google-suggest.
 
-input-name - the 'name' attribute of the input box.
+'input-name' - the 'name' attribute of the input box.
 
-localp - If nil, the completion will be done via an asynchronious call
-to the server. Otherwise, must be set to a list of items which will be
-sent to the client for local autocompletion. In this case, the value
-of 'fetch-action' is ignored. Note, if JavaScript is turned off local
+If 'resultset' is a list of items, suggestions will be done via local
+autocompletions cashed on the client. Otherwise, completion will be
+done via an asynchronious call to the server ('resultset' must be set
+to a function that accepts a search parameter and returns a list of
+itmes that match). Note, if JavaScript is turned off local
 autocompletion will degrade to a simple drowdown.
 
-value - can be used to set the value of the input box and/or dropdown.
+'default-value' - can be used to set the value of the input box and/or
+dropdown. If 'default-value' is a list, car of the list will be used
+to set the default value, and all subsequent elements will be used to
+test dropdown elements (in case suggest degrades to dropdown). This is
+useful because occassionally dropdown options have different names
+than intended default value.
 
-fetch-fn - when 'localp' is false, a function that accepts a single
-argument (the string entered by the user) and returns a list of items
-that will be sent to the client in an appropriate format.
-
-input-id, choices-id - optional IDs of input control and choice div,
+'input-id', 'choices-id' - optional IDs of input control and choice div,
 respectively. If an ID isn't provided, it will be generated.
 
-format-fn - a function used to format the results into html sent to
-the client. Accepts a list of results."
+'format-fn' - a function used to format the results into html sent to
+the client. Accepts a list of results.
+
+'welcome-name' - a welcome option in case dropdown is used (see
+'render-dropdown')."
+  (setf default-value (ensure-list default-value))
   (let ((a-input-name (attributize-name input-name)))
-    (if localp
+    (if (and (listp resultset)
+	     (not (ajax-request-p)))
+	(progn
+	  (render-dropdown a-input-name resultset :id input-id :selected-value default-value
+			   :welcome-name welcome-name)
+	  (with-javascript (if default-value
+			       "replaceDropdownWithSuggest(~A, '~A', '~A', '~A', '~A');"
+			       "replaceDropdownWithSuggest(~A, '~A', '~A', '~A');")
+	    (if welcome-name "true" "false") input-id a-input-name choices-id (car default-value)))
 	(with-html
-	  (:select :id input-id :name a-input-name
-		   (mapc (lambda (i)
-			   (if (string-equal i value)
-			       (htm (:option :selected "true" (str i)))
-			       (htm (:option (str i)))))
-			 localp))
-	  (with-javascript (if value
-			       "replaceDropdownWithSuggest('~A', '~A', '~A', '~A');"
-			       "replaceDropdownWithSuggest('~A', '~A', '~A');")
-	    input-id a-input-name choices-id value))
-	(with-html
-	  (:input :type "text" :id input-id :name a-input-name :class "suggest" :value value)
-	  (:div :id choices-id :class "suggest" "")
-	  (with-javascript "declareSuggest('~A', '~A', '~A', '~A');"
+	  (:input :type "text" :id input-id :name a-input-name :class "suggest" :value (car default-value)
+		  :maxlength max-length)
+	  (:div :id choices-id :class "suggest" :style "display: none" "")
+	  (with-javascript "declareSuggest('~A', '~A', ~A, '~A');"
 	    input-id choices-id
-	    (make-action
-	     (lambda (&rest keys)
-	       (funcall format-fn (funcall fetch-fn (request-parameter a-input-name)))))
+	    (if (listp resultset)
+		(encode-json-to-string resultset)
+		(format nil "'~A'"
+			(make-action
+			 (lambda (&rest keys)
+			   (funcall format-fn (funcall resultset (request-parameter a-input-name)))))))
 	    (session-name-string-pair))))))
 
 (defun format-suggest-list (results)
-  "Formats a list of results into
-HTML that can later be sent to a suggest control on the client."
+  "Formats a list of results into HTML that can later be sent to a
+suggest control on the client."
   (let ((*weblocks-output-stream* (make-string-output-stream)))
     (declare (special *weblocks-output-stream*))
     (with-html
